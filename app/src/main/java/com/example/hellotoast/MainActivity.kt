548f31e64f28926ae455de7feaa956ca2dfe0b92
nazwa yulianti munjana <yuliantimunjanan@gmail.com>
2025-09-24 00:01:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.hellotoast
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private var mCount = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mShowCount = findViewById<TextView>(R.id.show_count)
+        val buttonCountUp = findViewById<Button>(R.id.button_count)
+        val buttonToast = findViewById<Button>(R.id.button_toast)
+
+        buttonCountUp.setOnClickListener {
+            mCount++
+            mShowCount.text = mCount.toString()
+        }
+
+        buttonToast.setOnClickListener {
+            val tulisan = mShowCount.text.toString()
+            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
+        }
+    }
+}
