fd9bab44cdb4fb43bf5c3ea649497e35318903b0
nazwa yulianti <yuliantimunjanan@gmail.com>
2025-09-30 14:02:17 +0700

    tugas 2 ke 3

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.hellotoast
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -16,15 +19,22 @@ class MainActivity : AppCompatActivity() {
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
         val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
 
         buttonCountUp.setOnClickListener {
             mCount++
+            Log.d("MainActivity", "mCount=$mCount")
             mShowCount.text = mCount.toString()
         }
 
         buttonToast.setOnClickListener {
             val tulisan = mShowCount.text.toString()
-            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, getString(R.string.toast_message, tulisan), Toast.LENGTH_LONG).show()
+        }
+
+        buttonSwitchPage.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 }
